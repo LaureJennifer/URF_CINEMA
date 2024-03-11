887763f5fbde2d879af4f0244e9fdf3dceed9590
thieu do <whereyouat121196@gmail.com>
2024-03-11 12:17:30 +0700

    Update-FolderName and Mudblazor

--- BaseSolution.BlazorServer/Program.cs
+++ BaseSolution.BlazorServer/Program.cs
@@ -1,6 +1,7 @@
 using BaseSolution.BlazorServer.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 namespace BaseSolution.BlazorServer
 {
@@ -13,7 +14,7 @@ namespace BaseSolution.BlazorServer
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddMudServices();
 
             var app = builder.Build();
 
