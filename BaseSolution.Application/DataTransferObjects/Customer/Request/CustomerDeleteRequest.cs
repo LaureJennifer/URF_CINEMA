1205988fd20cd9379d57c4d0861ee35cfc96f9c6
thieu do <whereyouat121196@gmail.com>
2024-03-14 20:33:47 +0700

    Update-Db and Repo and màn Home

--- BaseSolution.Application/DataTransferObjects/Customer/Request/CustomerDeleteRequest.cs
+++ BaseSolution.Application/DataTransferObjects/Customer/Request/CustomerDeleteRequest.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaseSolution.Application.DataTransferObjects.Department.Request
+namespace BaseSolution.Application.DataTransferObjects.Customer.Request
 {
-    public class DepartmentDeleteRequest
+    public class CustomerDeleteRequest
     {
         public Guid Id { get; set; }
         public Guid? DeletedBy { get; set; }
---
+++ BaseSolution.Application/Interfaces/Repositories/ReadOnly/ISeatReadOnlyRepository.cs
@@ -0,0 +1,21 @@
+﻿using BaseSolution.Application.DataTransferObjects.Example.Request;
+using BaseSolution.Application.DataTransferObjects.Example;
+using BaseSolution.Application.ValueObjects.Pagination;
+using BaseSolution.Application.ValueObjects.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseSolution.Application.DataTransferObjects.Seat;
+using BaseSolution.Application.DataTransferObjects.Seat.Request;
+
+namespace BaseSolution.Application.Interfaces.Repositories.ReadOnly
+{
+    public interface ISeatReadOnlyRepository
+    {
+        Task<RequestResult<SeatDto?>> GetSeatByIdAsync(Guid idSeat, CancellationToken cancellationToken);
+        Task<RequestResult<PaginationResponse<SeatDto>>> GetSeatWithPaginationByAdminAsync(
+            ViewSeatWithPaginationRequest request, CancellationToken cancellationToken);
+    }
+}
