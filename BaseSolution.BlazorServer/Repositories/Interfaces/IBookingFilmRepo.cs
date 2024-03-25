a3f637cdc366fecaa60e2872f2e6a17532342c85
thieu do <whereyouat121196@gmail.com>
2024-03-25 21:49:57 +0700

    update-ghép api

---
+++ BaseSolution.BlazorServer/Repositories/Interfaces/IBookingFilmRepo.cs
@@ -0,0 +1,14 @@
+﻿using BaseSolution.BlazorServer.Data;
+using BaseSolution.BlazorServer.Data.DataTransferObjects.Film;
+using BaseSolution.BlazorServer.Data.DataTransferObjects.Film.Request;
+using BaseSolution.BlazorServer.Data.DataTransferObjects.FilmSchedule.Request;
+using BaseSolution.BlazorServer.ValueObjects.Response;
+
+namespace BaseSolution.BlazorServer.Repositories.Interfaces
+{
+    public interface IBookingFilmRepo
+    {
+        public Task<RequestResult<FilmDto>> GetByIdAsync(string id);
+        public Task<FilmScheduleListWithPaginationViewModel> GetAllFilmSchedule(ViewFilmScheduleWithPaginationRequest request);
+    }
+}
---
+++ BaseSolution.BlazorServer/Repositories/Interfaces/IFilmRepositories.cs
@@ -0,0 +1,10 @@
+﻿using BaseSolution.BlazorServer.Data;
+using BaseSolution.BlazorServer.Data.DataTransferObjects.Film.Request;
+
+namespace BaseSolution.BlazorServer.Repositories.Interfaces
+{
+    public interface IFilmRepositories
+    {
+        public Task<FilmListWithPaginationViewModel> GetAllActive(ViewFilmWithPaginationRequest request);
+    }
+}
