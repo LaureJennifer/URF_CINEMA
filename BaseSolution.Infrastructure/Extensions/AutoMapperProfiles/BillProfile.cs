3fbc714cadec5880bc96bb12235aa901ecf60ced
LaureJennifer <141312715+LaureJennifer@users.noreply.github.com>
2024-03-31 00:17:04 +0700

    update BE thống kê

--- BaseSolution.Infrastructure/Extensions/AutoMapperProfiles/BillProfile.cs
+++ BaseSolution.Infrastructure/Extensions/AutoMapperProfiles/BillProfile.cs
@@ -4,6 +4,7 @@ using BaseSolution.Application.DataTransferObjects.Film;
 using BaseSolution.Domain.Entities;
 using BaseSolution.Application.DataTransferObjects.Bill;
 using BaseSolution.Application.DataTransferObjects.Bill.Request;
+using BaseSolution.Application.DataTransferObjects.Ticket;
 
 namespace BaseSolution.Infrastructure.Extensions.AutoMapperProfiles
 {
@@ -15,6 +16,31 @@ namespace BaseSolution.Infrastructure.Extensions.AutoMapperProfiles
                .ForMember(des => des.CustomerName, opt => opt.MapFrom(x => x.CustomerEntity.Name))
                .ForMember(des => des.TotalPrice1, opt => opt.MapFrom(src => src.Tickets.Select(x=>x.BookingEntity).Sum(x=>x.SeatEntity.Price)))
                .ForMember(des => des.TicketQuantity1, opt => opt.MapFrom(src => src.Tickets.Select(x => x.BookingEntity).Count()));
+
+            CreateMap<BillEntity, BillStatisticForMonthDto>()
+                .ForMember(des => des.Month, opt => opt.MapFrom(x => x.CreatedTime.Month))
+                .ForMember(des => des.Year, opt => opt.MapFrom(x => x.CreatedTime.Year))
+                .ForMember(des => des.Revenue, opt => opt.MapFrom(x => x.TotalPrice));
+
+            CreateMap<BillEntity, BillStatisticForQuarterDto>()
+                .ForMember(des => des.Quarter, opt => opt.MapFrom(x => (x.CreatedTime.Month-1)/3+1))
+                .ForMember(des => des.Year, opt => opt.MapFrom(x => x.CreatedTime.Year))
+                .ForMember(des => des.Revenue, opt => opt.MapFrom(x => x.TotalPrice));
+
+            CreateMap<BillEntity, BillStatisticForYearDto>()
+               .ForMember(des => des.Year, opt => opt.MapFrom(x => x.CreatedTime.Year))
+               .ForMember(des => des.Revenue, opt => opt.MapFrom(x => x.TotalPrice));
+
+            CreateMap<BillEntity, TicketStatisticForMonthDto>()
+               .ForMember(des => des.Month, opt => opt.MapFrom(x => x.CreatedTime.Month))
+               .ForMember(des => des.Year, opt => opt.MapFrom(x => x.CreatedTime.Year))
+               .ForMember(des => des.Quantity, opt => opt.MapFrom(x => x.TicketQuantity));
+
+            CreateMap<BillEntity, TicketStatisticForQuarterDto>()
+                .ForMember(des => des.Quarter, opt => opt.MapFrom(x => (x.CreatedTime.Month - 1) / 3 + 1))
+                .ForMember(des => des.Year, opt => opt.MapFrom(x => x.CreatedTime.Year))
+                .ForMember(des => des.Quantity, opt => opt.MapFrom(x => x.TicketQuantity));
+
             CreateMap<BillCreateRequest, BillEntity>();
             CreateMap<BillUpdateRequest, BillEntity>();
 
---
+++ BaseSolution.Infrastructure/Implements/Repositories/ReadOnly/BillStatisticReadOnlyRespository.cs
@@ -0,0 +1,149 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BaseSolution.Application.DataTransferObjects.Bill;
+using BaseSolution.Application.DataTransferObjects.Bill.Request;
+using BaseSolution.Application.Interfaces.Repositories.ReadOnly;
+using BaseSolution.Application.Interfaces.Services;
+using BaseSolution.Application.ValueObjects.Common;
+using BaseSolution.Application.ValueObjects.Response;
+using BaseSolution.Domain.Enums;
+using BaseSolution.Infrastructure.Database.AppDbContext;
+using BaseSolution.Infrastructure.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSolution.Infrastructure.Implements.Repositories.ReadOnly
+{
+    public class BillStatisticReadOnlyRespository : IBillStatisticReadOnlyRespository
+    {
+        private readonly AppReadOnlyDbContext _dbContext;
+        private readonly ILocalizationService _localizationService;
+        private readonly IMapper _mapper;
+        public BillStatisticReadOnlyRespository(AppReadOnlyDbContext dbContext, ILocalizationService localizationService, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _localizationService = localizationService;
+            _mapper = mapper;
+        }
+        public async Task<RequestResult<List<BillStatisticForMonthDto>>> GetBillStasticForMonthAsync(BillStatisticRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var query = await _dbContext.BillEntities.Where(x => x.Status != EntityStatus.Deleted).ProjectTo<BillStatisticForMonthDto>(_mapper.ConfigurationProvider).PaginateAsync(request, cancellationToken);
+
+                List<BillStatisticForMonthDto> billStatistics = query.Data!.ToList();
+
+                List<BillStatisticForMonthDto> totalAmountForMonth = null;
+
+                if (billStatistics != null)
+                {
+
+                    totalAmountForMonth = billStatistics
+                            .GroupBy(x => new { x.Month, x.Year })
+                            .Select(g => new BillStatisticForMonthDto
+                            {
+                                Month = g.Key.Month,
+                                Year = g.Key.Year,
+                                Revenue = g.Sum(x => x.Revenue)
+                            }).ToList();
+
+                }
+                return RequestResult<List<BillStatisticForMonthDto>>.Succeed(totalAmountForMonth);
+
+            }
+            catch (Exception e)
+            {
+
+                return RequestResult<List<BillStatisticForMonthDto>>.Fail(_localizationService["Bill statistic for month is not found"], new[]
+                    {
+                    new ErrorItem
+                    {
+                        Error = e.Message,
+                        FieldName = LocalizationString.Common.FailedToGet + "bill statistic for month"
+                    }
+                });
+            }
+        }
+
+        public async Task<RequestResult<List<BillStatisticForQuarterDto>>> GetBillStasticForQuarterAsync(BillStatisticRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var query = await _dbContext.BillEntities.Where(x => x.Status != EntityStatus.Deleted).ProjectTo<BillStatisticForQuarterDto>(_mapper.ConfigurationProvider).PaginateAsync(request, cancellationToken);
+
+                List<BillStatisticForQuarterDto> billStatistics = query.Data!.ToList();
+
+                List<BillStatisticForQuarterDto> totalAmountForQuarter = null;
+
+                if (billStatistics != null)
+                {
+
+                    totalAmountForQuarter = billStatistics
+                            .GroupBy(x => new { x.Quarter, x.Year })
+                            .Select(g => new BillStatisticForQuarterDto
+                            {
+                                Quarter = g.Key.Quarter,
+                                Year = g.Key.Year,
+                                Revenue = g.Sum(x => x.Revenue)
+                            }).ToList();
+
+                }
+                return RequestResult<List<BillStatisticForQuarterDto>>.Succeed(totalAmountForQuarter);
+
+            }
+            catch (Exception e)
+            {
+
+                return RequestResult<List<BillStatisticForQuarterDto>>.Fail(_localizationService["Bill statistic for quarter is not found"], new[]
+                    {
+                    new ErrorItem
+                    {
+                        Error = e.Message,
+                        FieldName = LocalizationString.Common.FailedToGet + "bill statistic for quarter"
+                    }
+                });
+            }
+        }
+
+        public async Task<RequestResult<List<BillStatisticForYearDto>>> GetBillStasticForYearAsync(BillStatisticRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var query = await _dbContext.BillEntities.Where(x => x.Status != EntityStatus.Deleted).ProjectTo<BillStatisticForYearDto>(_mapper.ConfigurationProvider).PaginateAsync(request, cancellationToken);
+
+                List<BillStatisticForYearDto> billStatistics = query.Data!.ToList();
+
+                List<BillStatisticForYearDto> totalAmountForYear = null;
+
+                if (billStatistics != null)
+                {
+
+                    totalAmountForYear = billStatistics.GroupBy(x => x.Year)
+                                   .Select(g => new BillStatisticForYearDto
+                                   {
+                                       Year = g.Key,
+                                       Revenue = g.Sum(x => x.Revenue)
+                                   }).ToList();
+
+                }
+                return RequestResult<List<BillStatisticForYearDto>>.Succeed(totalAmountForYear);
+
+            }
+            catch (Exception e)
+            {
+
+                return RequestResult<List<BillStatisticForYearDto>>.Fail(_localizationService["Bill statistic for year is not found"], new[]
+                    {
+                    new ErrorItem
+                    {
+                        Error = e.Message,
+                        FieldName = LocalizationString.Common.FailedToGet + "bill statistic for year"
+                    }
+                });
+            }
+        }
+    }
+}
