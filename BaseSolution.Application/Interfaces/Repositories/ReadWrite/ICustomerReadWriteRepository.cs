549ab747a21e49fed721923dbd905be854ab5dd6
thieu do <whereyouat121196@gmail.com>
2024-04-09 19:11:06 +0700

    update- register

--- BaseSolution.Application/Interfaces/Repositories/ReadWrite/ICustomerReadWriteRepository.cs
+++ BaseSolution.Application/Interfaces/Repositories/ReadWrite/ICustomerReadWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using BaseSolution.Application.DataTransferObjects.Customer.Request;
+﻿using BaseSolution.Application.DataTransferObjects.Account;
+using BaseSolution.Application.DataTransferObjects.Customer.Request;
 using BaseSolution.Application.DataTransferObjects.Example.Request;
 using BaseSolution.Application.ValueObjects.Response;
 using BaseSolution.Domain.Entities;
@@ -15,5 +16,6 @@ namespace BaseSolution.Application.Interfaces.Repositories.ReadWrite
         Task<RequestResult<Guid>> AddCustomerAsync(CustomerEntity entity, CancellationToken cancellationToken);
         Task<RequestResult<int>> UpdateCustomerAsync(CustomerEntity entity, CancellationToken cancellationToken);
         Task<RequestResult<int>> DeleteCustomerAsync(CustomerDeleteRequest request, CancellationToken cancellationToken);
+        Task<RequestResult<Guid>> RegisterCustomerAsync(CustomerEntity request, CancellationToken cancellationToken);
     }
 }
